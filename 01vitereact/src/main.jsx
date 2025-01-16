40446bf0d453deb40d2b6eab7f3575bc8dbffa6a
Ankit Ashokbhai Anghan <53566784+ankit7588@users.noreply.github.com>
2025-01-16 16:40:09 +0000

    customreact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google'
+)
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visist Google
+  </a>
+)
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  reactElement
 )
